0b0c295b58bd6308f2a0a698c3d12fa6b12c66f1
105gun <ooo693428@163.com>
2024-11-16 17:02:31 +0800

    Init empty mod

--- Plugin.cs
+++ Plugin.cs
@@ -2,15 +2,15 @@
 using HarmonyLib;
 using UnityEngine;
 
-namespace ExampleMod;
+namespace MusicCancelMod;
 
-[BepInPlugin("yourname.example.mod", "Example Mod", "1.0.0.0")]
+[BepInPlugin("105gun.musiccancel.mod", "Music Cancel Mod", "1.0.0.0")]
 public class Plugin : BaseUnityPlugin
 {
     private void Start()
     {
         System.Console.WriteLine("Hello World from Elin Example Mod!");
-        var harmony = new Harmony("yourname.example.mod");
+        var harmony = new Harmony("105gun.musiccancel.mod");
         harmony.PatchAll();
     }
 }
